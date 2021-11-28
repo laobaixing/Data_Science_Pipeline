75a6b4d8c81d0812ded2405c7a2e04a9479d4e2c
laobaixing <ling@LAPTOP-NVMQ1RB9.localdomain>
2021-11-28 11:42:52 -0500

    update

---
+++ data_processing/extract_from_web.py
@@ -0,0 +1,61 @@
+"""
+This code define the functions for extracting data from XXX website 
+ 1. Set url to extract historical data,quote 
+ 2. Load as json 
+ 3. Change json to table
+ 4. merge and append the dataset
+
+Created on Fri Nov 26 13:05:13 2021
+
+@author: Shiyun Ling
+"""
+
+import requests
+import json
+import pandas as pd
+from datetime import date
+
+def download_stock(endpoint, stock, td_consumer_key, period):
+    full_url = endpoint.format(stock_ticker= stock,periodType='year',
+                               period=period,frequencyType='daily',frequency=1)
+    page = requests.get(url=full_url,
+                    params={'apikey' : td_consumer_key})
+    content = json.loads(page.content)
+
+    df = pd.json_normalize(content, record_path =['candles'], 
+                           meta =['symbol','empty'], meta_prefix='',
+                           record_prefix='')
+
+    df["datetime"] = pd.to_datetime(df['datetime'], unit="ms").dt.date
+    return df
+
+def download_quote(endpoint, stock, td_consumer_key):
+    full_url = endpoint.format(stock_ticker= stock)
+    page = requests.get(url=full_url,
+                    params={'apikey' : td_consumer_key})
+    df_quote = pd.read_json(page.content)
+    return df_quote
+
+def get_TD_stock_info(stock, period, history_data_link, quote_link, td_consumer_key):
+    
+    df_history = download_stock(history_data_link,stock, td_consumer_key, period)
+    df_history = df_history.drop(columns=["empty"])
+    df_quote = download_quote(quote_link, stock, td_consumer_key)
+    
+    if stock == '$SPX.X' or stock == '$COMPX':
+        today_info = df_quote.loc[ ['openPrice', 'highPrice', 'lowPrice', 
+                                    'lastPrice', 'totalVolume'], stock]
+    else:  
+        today_info = df_quote.loc[ ['openPrice', 'highPrice', 'lowPrice', 
+                                    'regularMarketLastPrice', 'totalVolume'], stock]
+    today_info = today_info.to_list()
+    df = df_history.append({"open":today_info[0], "high":today_info[1], 
+                            "low":today_info[2], 
+                            "close": today_info[3], "volume": today_info[4], 
+                            "datetime":pd.to_datetime(date.today()).date(), 
+                            "symbol":stock}, ignore_index=True)
+    
+    df.index = df["datetime"]  
+    print("The most recent date is:" + str(max(df["datetime"])))
+    
+    return df
---
+++ model_building/mixed_LM.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Build the model through Mixed linear
+
+Input:
+    data/stock_price.csv
+    
+Output:
+    
+
+Created on Sat Nov 27 10:43:58 2021
+
+@author: Ling
+"""
+
+import pandas as pd
+import statsmodels.api as sm
+
+from datetime import datetime
+import os
+
+basic_folder = os.getcwd()
+basic_folder = basic_folder.split("\\")
+basic_folder = "/".join(basic_folder[:3]+['OneDrive','stock_analysis'])
+os.chdir(basic_folder)
+
+stocks_df = pd.read_csv("data/stock_price.csv")
+
+# In[Model training]
+stocks_df['datetime'] = pd.to_datetime(stocks_df['datetime']).dt.date
+
+df_train = stocks_df[(stocks_df['datetime'] < 
+                      datetime.strptime('2021-3-31', "%Y-%m-%d").date())]
+df_val = stocks_df[(stocks_df['datetime'] >= 
+                    datetime.strptime('2021-3-31', "%Y-%m-%d").date())]
+
+# use its own normalized base line to represent recent emotion.
+df_train['RSI_cen'] = df_train.groupby('symbol').RSI.transform(lambda x: x - x.mean())
+df_val['RSI_cen'] = df_val.groupby('symbol').RSI.transform(lambda x: x - x.mean())
+
+features = ['average_13_60_down',"RSI_cen" ] #"MACD", 
+
+df_train = df_train.dropna(subset = ['price_change_forward_ratio'] + features )
+df_val = df_val.dropna(subset = ['price_change_forward_ratio'] + features )
+
+df_train = df_train.loc[~df_train.symbol.isin(["BB", "QS", "AI", "MAXR", "PLTR"])]
+df_val = df_val[~df_val.symbol.isin(["BB", "QS", "AI", "MAXR", "PLTR"])]
+
+# This model is only on the slope
+formula = 'price_change_forward_ratio ~' + "+".join(features)
+mlm_mod = sm.MixedLM.from_formula(
+    formula = formula, 
+    groups = 'symbol', 
+    re_formula = "~RSI_ind + SAR_diff_ind",
+    data = df_train
+)
+
+# Run the fit
+mlm_result = mlm_mod.fit(method=["lbfgs"])
+
+print(mlm_result.summary())
+
+# get the random effects coefs
+mlm_result.random_effects
+
+
+# In[Output the model result and prediction]
+val_predict = pd.DataFrame({'prediction':mlm_result.predict(df_val)})
+val_predict = pd.concat([df_val[['datetime', 'price_change_forward_ratio']], 
+                               val_predict], axis = 1)
+
+tra_predict = pd.DataFrame({'prediction':mlm_result.predict(df_train)})
+tra_predict = pd.concat([df_train[['datetime', 'price_change_forward_ratio']], 
+                               tra_predict], axis = 1)
+
+val_predict.to_csv("data/mixed_lm_val_pred.csv")
+tra_predict.to_csv("data/mixed_lm_tra_pred.csv")
+
+
